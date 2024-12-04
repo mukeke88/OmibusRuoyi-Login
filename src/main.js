cab41dc739a88df78444faadcaa836681d9465c5
mukeke88 <makaikai0628@gmail.com>
2024-12-04 14:35:45 +0800

    unsuredWork

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 import router from "./router";
 import store from "./store"; // Import the Vuex store
 
 const app = createApp(App);
 app.use(router);
 app.use(store); // Use the Vuex store
+app.use(ElementPlus);
 app.mount("#app");
